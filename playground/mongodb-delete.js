171102cdb7fea7a74ad9c159977ede5d4940790a
Alex Kiryxa <kiryxa@Alexs-iMac.local>
2016-12-09 14:50:31 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// let user = {name: 'Alex', age:30}; // DESTRUCTIRING
+// let {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if ( err ) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteMany({name: 'Alex'}).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID('584a9597cac7f5231879ba6c')
+    }).then((result) => {
+        console.log(result);
+    });
+    // db.close();
+});
